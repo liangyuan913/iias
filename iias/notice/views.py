d9cb7d0d993307636549e5d73b42c7a98da85aba
liangyuan913 <liangyuanliu9chsi@gmail.com>
2023-02-01 15:54:06 +0800

    Y230201 push

--- iias/notice/views.py
+++ iias/notice/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from notice.models import Notice, Comment
 
 # Create your views here.
 
@@ -6,5 +7,9 @@ def notice(request):
     '''
     Render the notice page
     '''
-    context = {'like': '公告事項請留意；錯過可能出問題！' }
+    # notices = Notice.objects.all()
+    notices = {notice:Comment.objects.filter(notice=notice) for notice in Notice.objects.all()}
+    
+    # context = {'like': '公告事項請留意；錯過可能出問題！' }
+    context = {'like': '公告事項請留意；錯過可能出問題！','notices':notices}
     return render(request, 'notice/notice.html', context)
